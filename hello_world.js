4c4ba0aca310ba05c7bfef720c8c491c355ddc70
Song Wei <1983.song.wei@gmail.com>
2017-01-30 18:12:00 -0500

    bug fix and higher order functions

--- hello_world.js
+++ hello_world.js
@@ -1,12 +1,11 @@
 function upperCaser(input) {
     function helper(acc, input) {
-	const sub = input.substr(1);
-	if ( !sub ) {
+	if (0 == input.length ) {
 	    return acc;
 	}
-	return helper(acc + input.charAt(0).toUpperCase(), sub);
+	return helper(acc + input.charAt(0).toUpperCase(), input.substr(1));
     }
-    return helper("", input);
+    return helper("", ''+input);
 }
 
 module.exports = upperCaser;
